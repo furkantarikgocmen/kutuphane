c58fbb8b5f2844f542763cd8ed623835bbac0a0d
furkantarikgocmen <f.tarikgocmen@gmail.com>
2020-11-11 17:33:02 +0300

    Role and Account Entities added

---
+++ src/main/java/com/ftg/kutuphane/entitiy/Account.java
@@ -0,0 +1,35 @@
+package com.ftg.kutuphane.entitiy;
+
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = account, schema = "\"kutuphane_schema\"")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "account_id")
+    private UUID id;
+
+    @Column(name = "user_name", unique = true)
+    @NotEmpty(message = "Please provide an User Name")
+    private String userName;
+
+    @Column(name = "password")
+    @NotEmpty(message = "Please provide your password")
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    //todo: Email, Telephone, name-lastName, active-inactive props.
+}
---
+++ src/main/java/com/ftg/kutuphane/entitiy/Role.java
@@ -0,0 +1,23 @@
+package com.ftg.kutuphane.entitiy;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = role, schema = "\"kutuphane_schema\"")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Role name must not be empty")
+    private String name;
+}
